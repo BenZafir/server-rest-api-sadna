0727a3c19d236cd2a8cb9b70c840d168cd4c22ed
Eden <eden.refael@auroralabs.com>
2022-11-13 11:53:39 +0200

    jwt support first step

--- src/controllers/loggin.controller.ts
+++ src/controllers/loggin.controller.ts
@@ -1,25 +1,32 @@
 import { Handler } from 'express';
 import { getConnection } from '../database';
 
+require("dotenv").config();
 
 export const loggin: Handler = (req, res) => {
   try {
+    const jwt = require('jsonwebtoken');
     const validKeys = ['userName', 'password'];
     if (Object.keys(req.body).every(key => validKeys.includes(key))) {
       const { userName, password } = req.body;
 
-      let user = getConnection().get('users').find({ name: req.body.userName }).value();
-      if (!user) {
-        res.status(401).json({ "message": "bad username or password" });
+      let user = getConnection().get('users').find({ name: req.body.userName }).value(); // will get the user
+      if (!user) { //if user not exists unauthorized user
+        res.status(401).json({ "message": "bad username or password" }); 
         return;
       }
-      if (user.password == req.body.password) {
-        let token = "token"
-        res.json(token);
-        return;
-      }
+      // if the password of the user is equal too we'll generate a token and send it to the user
+      // authorization:
+      if (user.password == req.body.password) { 
+        const id = user.id;
+        const jwt_secret = process.env.JWT_SECRET;
+        const token = jwt.sign({id}, jwt_secret);
+        // token = "token" 
+        res.json(token); 
+        return; 
+      } 
       else{
-        res.status(401).json({ "message": "bad username or password" });
+        res.status(401).json({ "message": "bad username or password" }); 
         return;
       }
     } else {

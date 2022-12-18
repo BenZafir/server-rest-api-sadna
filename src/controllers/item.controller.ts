a1ecafa2a6cb669c69991644f8e62f6bdda38e75
ben <ben@gmail.com>
2022-12-18 19:33:19 +0200

    small fix in item controller

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,7 +1,7 @@
 import { Handler } from 'express';
 import { nanoid } from 'nanoid';
 import { getConnection } from '../database';
-import  { isUserAllowed } from '../auth';
+import { isUserAllowed } from '../auth';
 
 export const getItems: Handler = async (req, res) => {
 
@@ -22,15 +22,14 @@ export const getItem: Handler = async (req, res) => {
 export const createItem: Handler = async (req, res) => {
   const adminPermissionRequired = true;
   const auth = await isUserAllowed(req.headers.authorization, adminPermissionRequired);
-  if(!auth)
-    return res.status(404).json({"message": "wronge token"});
+  if (!auth)
+    return res.status(404).json({ "message": "wronge token" });
   try {
-    const validKeys = ['name', 'id', 'imageUrl', 'category', 'price' ];
+    const validKeys = ['name', 'id', 'imageUrl', 'category', 'price'];
     if (Object.keys(req.body).every(key => validKeys.includes(key))) {
-      const { name, imageUrl, category, price} = req.body;
+      const { name, imageUrl, category, price } = req.body;
       const categoryCheck = getConnection().get('categories').find({ name: category }).value();
-      if(!categoryCheck)
-      {
+      if (!categoryCheck) {
         res.status(400).json({ "message": "bad request" });
         return;
       }
@@ -54,17 +53,16 @@ export const createItem: Handler = async (req, res) => {
 export const updateItem: Handler = async (req, res) => {
   const adminPermissionRequired = true
   const auth = await isUserAllowed(req.headers.authorization, adminPermissionRequired);
-  if(!auth)
-    return res.status(404).json({"message": "wronge token"});
+  if (!auth)
+    return res.status(404).json({ "message": "wronge token" });
 
   const item = getConnection().get('items').find({ id: req.params.id }).value();
   if (!item) {
     return res.status(404).json({ "message": "categorie doesnt exists" });
   }
-   else {
+  else {
     const categoryCheck = getConnection().get('categories').find({ name: req.body.category }).value();
-    if(!categoryCheck)
-    {
+    if (!categoryCheck) {
       res.status(400).json({ "message": "bad request" });
       return;
     }
@@ -77,8 +75,8 @@ export const updateItem: Handler = async (req, res) => {
 export const deleteItem: Handler = async (req, res) => {
   const adminPermissionRequired = true;
   const auth = await isUserAllowed(req.headers.authorization, adminPermissionRequired);
-  if(!auth)
-    return res.status(404).json({"message": "wronge token"});
+  if (!auth)
+    return res.status(404).json({ "message": "wronge token" });
   const categorie = getConnection().get('items').find({ id: req.params.id }).value();
   if (!categorie) {
     return res.status(404).json({ "message": "categorie doesnt exists" });
